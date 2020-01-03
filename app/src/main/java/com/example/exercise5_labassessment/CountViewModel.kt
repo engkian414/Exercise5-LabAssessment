c46b4634dd46ca74745c97c133793f9e6e6b07e5
Engkian <ekng1999@gmail.com>
2020-01-03 12:52:33 +0800

    Committed by Engkian

---
+++ app/src/main/java/com/example/exercise5_labassessment/CountViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.exercise5_labassessment
+
+
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel() {
+    var countLike: Int = 0
+    var countDislike: Int = 0
+}
